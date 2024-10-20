1d9540a71c93e522dce62d7a64702ddd484f6754
oussama.benyssef <oussama.benyssef@gmail.com>
2024-10-20 17:36:37 +0100

    addQuote function

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+
     let newDiv = document.createElement("div");
     let quoteCategory = document.createElement("h3");
     let quoteParagraph = document.createElement("p");
@@ -8,26 +9,34 @@ document.addEventListener("DOMContentLoaded", function() {
     newDiv.appendChild(quoteCategory);
     newDiv.appendChild(quoteParagraph);
     document.body.insertBefore(newDiv, showBtn);
+    let Quotes = [{text : "The measure of success is not how much time you spend doing what you love, it's how little time you spend doing what you hate.", category :"Success"},
+                {text : "Having knowledge but lacking the power to express it clearly is no better than never having any ideas at all.", category : "Education"},
+                {text : "The source of wisdom is whatever is going to happen to us today.", category : "Wisdom"}];
 
+    function displayRandomQuote(){
+            let i = Math.floor(Math.random() * (Quotes.length));
+            quoteCategory.textContent = Quotes[i].category;
+            quoteParagraph.textContent = Quotes[i].text;
+    }
 
-    let quote1 = {text : "The measure of success is not how much time you spend doing what you love, it's how little time you spend doing what you hate.", category :"Success"};
-    let quote2 = {text : "Having knowledge but lacking the power to express it clearly is no better than never having any ideas at all.", category : "Education"};
-    let quote3 = {text : "The source of wisdom is whatever is going to happen to us today.", category : "Wisdom"};
-    let Quotes = [quote1, quote2, quote3];
-
-    function displayRandomQuote(Q){
-            Math.floor(Math.random() * (Q.length));
-            let i = Math.floor(Math.random() * (Q.length));
-            quoteCategory.textContent = Q[i].category;
-            quoteParagraph.textContent = Q[i].text;
-            console.log (Q[i].text);
-
+    function addQuote(){
+    let newQuoteCategory = document.getElementById("newQuoteCategory").value;
+    let newQuoteText = document.getElementById("newQuoteText").value;
+    if( newQuoteCategory !="" && newQuoteText !=""){
+    Quotes.push({text : newQuoteText, category : newQuoteCategory });
+    document.getElementById("newQuoteCategory").value = "";
+    document.getElementById("newQuoteText").value = "";
+    alert("Quote added successfuly");
+    console.log(Quotes);
+    }
+    else {
+        alert("Fields should not be empty");
+    }
     }
 
 
-    showBtn.addEventListener("click", function () {
-        displayRandomQuote(Quotes);
-     });
+    document.getElementById('showQuoteBtn').addEventListener("click", displayRandomQuote);
 
+     document.getElementById('addQuoteBtn').addEventListener('click', addQuote);
 
 });
